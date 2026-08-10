81a85c68c3f70a1d83ca4b64017da2ff349a078b
ktxsubway-bit <ktxsubway@gmail.com>
2026-08-10 10:27:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
-const CONFIG = {
-    ADMIN_PASSWORD: "ilikesuyu",
-    OTP_SECRET: "QGNJGFTDCKLPDSER" // 16자리 이상의 Base32 시크릿 키
-};
+(function () {
+    // 1. 변수 전체를 즉시 실행 함수 안으로 숨깁니다.
+    const CONFIG = {
+        'ADMIN_PASSWORD': 'ilikesuyu',
+        'OTP_SECRET': 'QGNJGFTDCKLPDSER'
+    };
+    
+    // 2. 외부(HTML 등)에서 가져다 쓸 수 있도록 globalThis(window)에 등록합니다.
+    globalThis['CONFIG'] = CONFIG;
+})();
